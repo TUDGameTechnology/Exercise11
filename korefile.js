42fef72b470452b72a56450af234b59c3507f9b9
Florian Mehm <florian@mehm.net>
2016-12-21 10:35:00 +0100

    Added cpp11-flag to korefile.

--- korefile.js
+++ korefile.js
@@ -2,6 +2,7 @@ let project = new Project('Exercise12', __dirname);
 
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
+project.cpp11 = true;
 
 Project.createProject('Kore', __dirname).then((kore) => {
 	project.addSubProject(kore);
