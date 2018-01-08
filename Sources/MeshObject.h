c3f4feabf1268c48cc4ce4a31d2d14ab471bfc39
Polona Caserman <polona.caserman@gmail.com>
2018-01-08 11:44:20 +0100

    Update Kore

---
+++ Sources/MeshObject.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  Exercise12
+//
+//  Created by Polona Caserman on 08.01.18.
+//  Copyright © 2018 KTX Software Development. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
